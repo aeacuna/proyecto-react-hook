661faa77616891de54efb7a4f55c428b75138dd9
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-05-06 16:34:24 -0300

    Creo Proyecto React-Hooks

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Typography } from '@material-ui/core'
+
+const index = () => {
+	return (
+		<Container>
+			<Typography variant="h1">
+				Home
+		<	/Typography>
+		</Container>
+	)
+}
+
+export default index
