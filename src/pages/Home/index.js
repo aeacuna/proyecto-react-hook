9e6ad55c3bf157486720894df4047f1b6ec79ccc
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-05-08 16:56:22 -0300

    avances

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import { Container, Typography } from '@material-ui/core'
-
+import React, { useState } from 'react'
+import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core'
+import {MovieIcon} from '../../icons'
+import style from './style'
 const index = () => {
+	const [searchText, setSearchText] = useState('')
+	const handleSearchTextHandle = event => {
+		setSearchText(event.target.value)
+	}
+	const handleCleanTextClick = () => {
+		setSearchText('')
+	}
+	const handleSearchTextClick = () => {
+
+	}
+
+	const classes = style()
 	return (
-		<Container>
-			<Typography variant="h1">
-				Home
-		<	/Typography>
+		<Container className={classes.container}>
+			<Card className={classes.cardContainer}>
+				<Grid container className={classes.titleGridContainer}>
+					<Grid>
+						<Typography className={classes.title}>Bienvenido</Typography>
+					</Grid>
+					<Grid>
+						<MovieIcon className={classes.movieIcon}/>
+					</Grid>
+				</Grid>
+				<TextField 
+					value={searchText}
+					placeholder="Buscar..."
+					className={classes.textFieldSearch}
+					onChange={handleSearchTextHandle}/>
+				<Grid className={classes.buttonsContainer}>
+					<Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
+					<Button variant="contained" color="primary" size="large" className={classes.searchButton} onClick={handleSearchTextClick}>Buscar</Button>
+				</Grid>
+			</Card>
 		</Container>
 	)
 }
